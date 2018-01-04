352274c142378a01a4b6fea8eb421ef45407b087
James Jelenko <jamesjelenko@gmail.com>
2018-01-04 11:25:20 -0800

    ran webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,20 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-	entry: `${SRC_DIR/index.jsx}`,
+	entry: `${SRC_DIR}/index.jsx`,
 	output: {
 		filename: 'bundle.js',
 		path: DIST_DIR
 	},
 	module: {
-		include: SRC_DIR.
-		loader: 'babel-loader',
-		query: {
-			presets: ['react', 'es2015']
-		}
+		loaders: [
+			{
+        include : SRC_DIR,
+        loader : 'babel-loader',      
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
 	}
 }
