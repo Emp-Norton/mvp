a26a764287bb4e1ee1c16a9982ff158f453d4158
James Jelenko <jamesjelenko@gmail.com>
2018-01-04 11:17:25 -0800

    added webpack + config, created client directory.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+	entry: `${SRC_DIR/index.jsx}`,
+	output: {
+		filename: 'bundle.js',
+		path: DIST_DIR
+	},
+	module: {
+		include: SRC_DIR.
+		loader: 'babel-loader',
+		query: {
+			presets: ['react', 'es2015']
+		}
+	}
+}
