3af477e5fd13214cfb23f08517d138ef445042c7
James Jelenko <jamesjelenko@gmail.com>
2018-01-04 11:37:37 -0800

    added main index.html page to render app on

---
+++ client/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+
+class App extends React.Component {
+	render() {
+		return (
+
+			)
+	}
+}
+
+ReactDOM.render(App, document.getElementById('app'));
